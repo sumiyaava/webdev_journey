95aa3ac352e45d52224ea763c4f8e4da52ded4a1
Sumiya Rahaman Ava <sumaiyarahamanava11@gmail.com>
2026-08-13 14:33:50 +0600

    Add files via upload

---
+++ day 3/problem 1/script.js
@@ -0,0 +1,26 @@
+let a =20;
+let b =5;
+let operator ="*";
+
+switch (operator) {
+    case "+":
+        console.log(a+b);
+        
+        break;
+     case "-":
+        console.log(a-b);
+        break;
+
+      case "*":
+        console.log(a*b);
+        break;
+
+     case "/":
+        console.log(a/b);
+        break;   
+
+
+    default:
+        console.log("Invalid case");
+        
+}
